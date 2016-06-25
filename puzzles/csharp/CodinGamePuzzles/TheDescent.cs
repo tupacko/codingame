ae7ec7d6ad0c0268c9fade586ad892b25aed6cc7
tupacko <tupacko@gmail.com>
2016-06-25 23:28:31 +0300

    Solved the "The Descent" puzzle.

--- puzzles/csharp/CodinGamePuzzles/TheDescent.cs
+++ puzzles/csharp/CodinGamePuzzles/TheDescent.cs
@@ -1,1 +1,27 @@
-// Cannot get code from history of CodinGame
+using System;
+
+internal class TheDescent
+{
+	private static void Main(string[] args)
+	{
+		const int NUMBER_OF_MOUNTAINS = 8;
+
+		while (true)
+		{
+			var maxHeight = 0;
+			var attackPosition = 0;
+			for (int i = NUMBER_OF_MOUNTAINS; 0 != i; i--)
+			{
+				int height = int.Parse(Console.ReadLine());
+
+				if (maxHeight < height)
+				{
+					maxHeight = height;
+					attackPosition = NUMBER_OF_MOUNTAINS - i;
+				}
+			}
+
+			Console.WriteLine(attackPosition);
+		}
+	}
+}
