60b1897a331a2f7df83e7d982cf4b5ef83f8c16d
tupacko <tupacko@gmail.com>
2016-06-26 16:14:29 +0300

    Small clean-up around Temperatures.

--- puzzles/csharp/CodinGamePuzzles/Temperatures.cs
+++ puzzles/csharp/CodinGamePuzzles/Temperatures.cs
@@ -3,35 +3,34 @@ using System.Linq;
 
 internal class Temperatures
 {
+	private static void Main(string[] args)
+	{
+		int numberOfTemperatures = int.Parse(Console.ReadLine());
+		if (0 == numberOfTemperatures)
+		{
+			Console.WriteLine("0");
+			return;
+		}
+
+		string temperaturesText = Console.ReadLine();
+		var finder = new Finder(temperaturesText);
+
+		Console.Write(finder.FindResult());
+	}
+
 	private class Finder
 	{
 		public Finder(string temperatures)
 		{
-			if (string.IsNullOrWhiteSpace(temperatures))
-			{
-				this.temps = new int[0];
-				return;
-			}
-
-			this.temps = temperatures.Split(' ').Select(s => int.Parse(s)).ToArray();
+			this.temps = temperatures.Split(' ').Select(int.Parse).ToArray();
 		}
 
 		public int FindResult()
 		{
-			if (0 == this.temps.Length)
-			{
-				return 0;
-			}
+			var closestTwo = this.temps.OrderBy(Math.Abs).Take(2).ToArray();
+			var closest = closestTwo[0];
 
-			return FindClosestToZero();
-		}
-
-		private int FindClosestToZero()
-		{
-			var firstOrderedTemps = this.temps.OrderBy(t => Math.Abs(t)).Take(2).ToArray();
-			var closest = firstOrderedTemps[0];
-
-			if (ShouldTakePositive(firstOrderedTemps))
+			if (ShouldTakePositive(closestTwo))
 			{
 				return Math.Abs(closest);
 			}
@@ -50,15 +49,5 @@ internal class Temperatures
 		}
 
 		private readonly int[] temps;
-	}
-
-	private static void Main(string[] args)
-	{
-		int N = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
-		string TEMPS = Console.ReadLine(); // the N temperatures expressed as integers ranging from -273 to 5526
-
-		var finder = new Finder(TEMPS);
-
-		Console.Write(finder.FindResult());
 	}
 }
