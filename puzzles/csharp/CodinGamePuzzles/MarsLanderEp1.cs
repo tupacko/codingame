c3656492f40140b4b22e63275f94b7e4efbbda13
tupacko <tupacko@gmail.com>
2016-06-26 16:37:33 +0300

    Removed all redundant logic from the first episode of Mars Lander. Can be added when needed later.

--- puzzles/csharp/CodinGamePuzzles/MarsLanderEp1.cs
+++ puzzles/csharp/CodinGamePuzzles/MarsLanderEp1.cs
@@ -3,175 +3,67 @@ using System.Linq;
 
 internal class MarsLanderEp1
 {
-	private class Point
+	private static void Main(string[] args)
 	{
-		public Point(int x, int y)
-		{
-			X = x;
-			Y = y;
-		}
-
-		public int X
-		{
-			get;
-			private set;
-		}
-
-		public int Y
-		{
-			get;
-			private set;
-		}
-	}
-
-	private class Reader
-	{
-		public int Read()
-		{
-			return int.Parse(Console.ReadLine());
-		}
-
-		public int[] ReadArray()
-		{
-			return Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray();
-		}
-	}
-
-	private class MarsLander
-	{
-		public MarsLander(Point[] surface, int[] data)
-		{
-			this.surface = surface;
-
-			this.position = new Point(data[0], data[1]);
-			this.horizontalSpeed = data[2];
-			this.verticalSpeed = data[3];
-			this.fuel = data[4];
-			this.rotation = data[5];
-			this.power = data[6];
-		}
-
-		public void Move()
-		{
-			var rotation = CalculateRotation();
-			var power = CalculatePower();
-
-			Communicate(rotation, power);
-		}
-
-		private int CalculatePower()
-		{
-			//var landingDistance = GetCrashDistance();
-			//Console.Error.Write(landingDistance);
-
-			// more realistic would be: landingDistance > timeToDecelerateToVerticalLimit
-			if (Math.Abs(this.verticalSpeed) < VerticalSpeedLimit)
-			{
-				return 0;
-			}
-
-			return 4;
-		}
-
-		private int GetCrashDistance()
-		{
-			Point left = null;
-			Point right = null;
-
-			for (int i = 0, max = this.surface.Length; i < max; i++)
-			{
-				if (this.surface[i].X > this.position.X)
-				{
-					right = this.surface[i];
-					left = this.surface[i - 1];
-					break;
-				}
-			}
-
-			// straight landing, Y's are equal
-			if (left.Y != right.Y)
-			{
-				Console.Error.WriteLine("Error: Y's are not equal when straight landing!!!");
-			}
-
-			// Console.Error.WriteLine("{0}, {1} | {2}, {3} | {4}, {5}", left.X, left.Y,
-			// this.position.X, this.position.Y, right.X, right.Y);
-
-			return this.position.Y - right.Y;
-		}
-
-		private int CalculateRotation()
-		{
-			return 0; // straight landing
-		}
-
-		private void Communicate(int rotation, int power)
-		{
-			Console.WriteLine("{0} {1}", rotation, power);
-		}
-
-		private readonly Point[] surface;
-		private readonly Point position;
-		private readonly int horizontalSpeed;
-		private readonly int verticalSpeed;
-		private readonly int fuel;
-		private readonly int rotation;
-		private readonly int power;
-
-		private const int VerticalSpeedLimit = 40;
-		private const int HorizontalSpeedLimit = 20;
-
-		private const double GravityAcceleration = 3.711;
-	}
-
-	private MarsLanderEp1()
-	{
-		InitLocals();
-		InitSurface();
-		LoadSurface();
-	}
-
-	public void Play()
-	{
-		// game loop
+		TrashInitialData();
 		while (true)
 		{
-			var data = this.reader.ReadArray();
-			var currentTurn = new MarsLander(this.surface, data);
+			var data = ReadArray();
+			var currentTurn = new MarsLander(data);
 
 			currentTurn.Move();
 		}
 	}
 
-	private void InitLocals()
+	private static void TrashInitialData()
 	{
-		this.reader = new Reader();
-	}
-
-	private void InitSurface()
-	{
-		int N = this.reader.Read(); // the number of points used to draw the surface of Mars.
-		this.surface = new Point[N];
-	}
-
-	private void LoadSurface()
-	{
-		var points = this.surface.Length;
-		for (int i = 0; i < points; i++)
+		int initialDataCount = int.Parse(Console.ReadLine());
+		for (int i = 0; i < initialDataCount; i++)
 		{
-			var current = this.reader.ReadArray();
-
-			this.surface[i] = new Point(current[0], current[1]);
+			Console.ReadLine();
 		}
 	}
 
-	private static void Main(string[] args)
+	private static int[] ReadArray()
 	{
-		var player = new MarsLanderEp1();
-		player.Play();
+		return Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 	}
 
-	private Reader reader;
+	private class MarsLander
+	{
+		public MarsLander(int[] data)
+		{
+			this.verticalSpeed = data[VERTICAL_SPEED_INDEX];
+		}
 
-	private Point[] surface;
+		public void Move()
+		{
+			var power = CalculatePower();
+
+			Communicate(power);
+		}
+
+		private int CalculatePower()
+		{
+			if (Math.Abs(this.verticalSpeed) < VERTICAL_SPEED_LIMIT)
+			{
+				return 0;
+			}
+
+			return MAX_POWER;
+		}
+
+		private void Communicate(int power)
+		{
+			Console.WriteLine("0 {0}", power);
+		}
+
+		private readonly int verticalSpeed;
+
+		private const int VERTICAL_SPEED_INDEX = 3;
+
+		private const int VERTICAL_SPEED_LIMIT = 40;
+
+		private const int MAX_POWER = 4;
+	}
 }
