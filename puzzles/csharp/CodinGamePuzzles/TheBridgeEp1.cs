b0344ba39ae28cca3257c581144783a235651202
tupacko <tupacko@gmail.com>
2016-06-26 13:03:18 +0300

    Removed the chain of responsibility and moved behavior into Bike. Code is 50% shorter and does the same exact thing. Of course, there is a small loss of (unneeded flexibility).

--- puzzles/csharp/CodinGamePuzzles/TheBridgeEp1.cs
+++ puzzles/csharp/CodinGamePuzzles/TheBridgeEp1.cs
@@ -5,15 +5,13 @@ internal class TheBridgeEp1
 	private static void Main(string[] args)
 	{
 		var environment = Environment.Load();
-		var processorChain = CreateProcessors(environment);
 		var motorbike = new Bike();
 
 		// game loop
 		while (true)
 		{
 			motorbike.LoadState();
-
-			processorChain.Process(motorbike);
+			motorbike.Move(environment);
 		}
 	}
 
@@ -62,182 +60,57 @@ internal class TheBridgeEp1
 			this.position = int.Parse(Console.ReadLine());
 		}
 
-		public bool IsOver(Environment environment)
+		public void Move(Environment environment)
+		{
+			if (IsOver(environment))
+			{
+				Console.WriteLine("SLOW");
+				return;
+			}
+
+			if (ShouldJump(environment))
+			{
+				Console.WriteLine("JUMP");
+				return;
+			}
+
+			if (IsTooFast(environment))
+			{
+				Console.WriteLine("SLOW");
+				return;
+			}
+
+			if (!IsFastEnough(environment))
+			{
+				Console.WriteLine("SPEED");
+				return;
+			}
+
+			Console.WriteLine("WAIT");
+		}
+
+		private bool IsOver(Environment environment)
 		{
 			return environment.RoadLength + environment.GapLength <= this.position;
 		}
 
-		public bool IsFastEnough(Environment environment)
+		private bool ShouldJump(Environment environment)
 		{
-			return environment.GapLength >= this.speed;
+			return this.position + this.speed > environment.RoadLength;
 		}
 
-		public bool IsTooFast(Environment environment)
+		private bool IsTooFast(Environment environment)
 		{
 			return environment.GapLength + 1 < this.speed;
 		}
 
-		public bool ShouldJump(Environment environment)
+		private bool IsFastEnough(Environment environment)
 		{
-			return this.position + this.speed > environment.RoadLength;
+			return environment.GapLength < this.speed;
 		}
 
 		private int speed;
 
 		private int position;
-	}
-
-	private static ProcessorBase CreateProcessors(Environment environment)
-	{
-		var speed = new Speed(environment);
-		var slow = new Slow(environment);
-		var jump = new Jump(environment);
-		var wait = new Wait();
-
-		speed.SetNextInChain(jump);
-		jump.SetNextInChain(slow);
-		slow.SetNextInChain(wait);
-
-		return speed;
-	}
-
-	private interface IProcessor
-	{
-		void Process(Bike bike);
-	}
-
-	private abstract class ProcessorBase : IProcessor
-	{
-		protected ProcessorBase(Environment environment)
-		{
-			Environment = environment;
-		}
-
-		protected Environment Environment
-		{
-			get;
-			private set;
-		}
-
-		public void SetNextInChain(IProcessor next)
-		{
-			this.nextProcessor = next;
-		}
-
-		public void Process(Bike bike)
-		{
-			if (!CanHandle(bike))
-			{
-				HandleNext(bike);
-				return;
-			}
-
-			Handle();
-		}
-
-		protected abstract bool CanHandle(Bike bike);
-
-		protected abstract void Handle();
-
-		private void HandleNext(Bike bike)
-		{
-			if (ReferenceEquals(null, this.nextProcessor))
-			{
-				return;
-			}
-
-			this.nextProcessor.Process(bike);
-		}
-
-		private IProcessor nextProcessor;
-	}
-
-	private class Speed : ProcessorBase
-	{
-		public Speed(Environment environment) : base(environment)
-		{
-		}
-
-		protected override bool CanHandle(Bike bike)
-		{
-			if (bike.IsOver(Environment))
-			{
-				return false;
-			}
-
-			if (bike.IsFastEnough(Environment))
-			{
-				return true;
-			}
-
-			return false;
-		}
-
-		protected override void Handle()
-		{
-			Console.WriteLine("SPEED");
-		}
-	}
-
-	private class Slow : ProcessorBase
-	{
-		public Slow(Environment environment) : base(environment)
-		{
-		}
-
-		protected override bool CanHandle(Bike bike)
-		{
-			if (bike.IsOver(Environment))
-			{
-				return true;
-			}
-
-			if (bike.IsTooFast(Environment))
-			{
-				return true;
-			}
-
-			return false;
-		}
-
-		protected override void Handle()
-		{
-			Console.WriteLine("SLOW");
-		}
-	}
-
-	private class Jump : ProcessorBase
-	{
-		public Jump(Environment environment) : base(environment)
-		{
-		}
-
-		protected override bool CanHandle(Bike bike)
-		{
-			if (bike.IsOver(Environment))
-			{
-				return false;
-			}
-
-			if (bike.ShouldJump(Environment))
-			{
-				return true;
-			}
-
-			return false;
-		}
-
-		protected override void Handle()
-		{
-			Console.WriteLine("JUMP");
-		}
-	}
-
-	private class Wait : IProcessor
-	{
-		public void Process(Bike bike)
-		{
-			Console.WriteLine("WAIT");
-		}
 	}
 }
