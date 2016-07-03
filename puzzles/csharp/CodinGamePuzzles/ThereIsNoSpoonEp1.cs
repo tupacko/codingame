65d6cbcef1e1fd64d28f8cd2e0cfa22f5b6b5d9f
tupacko <tupacko@gmail.com>
2016-07-03 11:14:27 +0300

    Created draft for There Is No Spoon, Ep. 1

---
+++ puzzles/csharp/CodinGamePuzzles/ThereIsNoSpoonEp1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodinGamePuzzles
+{
+	internal class ThereIsNoSpoonEp1
+	{
+		private static void Main(string[] args)
+		{
+			int width = int.Parse(Console.ReadLine()); // the number of cells on the X axis
+			int height = int.Parse(Console.ReadLine()); // the number of cells on the Y axis
+			for (int i = 0; i < height; i++)
+			{
+				string line = Console.ReadLine(); // width characters, each either 0 or .
+			}
+
+			// Write an action using Console.WriteLine() To debug: Console.Error.WriteLine("Debug messages...");
+
+			// Three coordinates: a node, its right neighbor, its bottom neighbor
+			Console.WriteLine("0 0 1 0 0 1");
+		}
+	}
+}
