746b5d9550ede7b5fc4c3577e96598f937eedfba
tupacko <tupacko@gmail.com>
2016-07-03 11:53:23 +0300

    There Is No Spoon, Ep.1 - 87% done. Broken square is missing.

--- puzzles/csharp/CodinGamePuzzles/ThereIsNoSpoonEp1.cs
+++ puzzles/csharp/CodinGamePuzzles/ThereIsNoSpoonEp1.cs
@@ -7,6 +7,7 @@ namespace CodinGamePuzzles
 		private static void Main(string[] args)
 		{
 			var map = Map.Load();
+			map.ListNeighbours();
 		}
 
 		private class Map
@@ -16,7 +17,7 @@ namespace CodinGamePuzzles
 				this.width = width;
 				this.height = height;
 
-				this.nodes = new char[this.width][];
+				this.nodes = new char[this.height][];
 			}
 
 			public static Map Load()
@@ -35,14 +36,83 @@ namespace CodinGamePuzzles
 				for (int i = 0; i < height; i++)
 				{
 					string line = Console.ReadLine();
+
 					this.nodes[i] = line.ToCharArray();
 				}
 			}
 
 			public void ListNeighbours()
 			{
-				// Three coordinates: a node, its right neighbor, its bottom neighbor
-				Console.WriteLine("0 0 1 0 0 1");
+				for (int i = 0; i < this.width; i++)
+				{
+					for (int j = 0; j < this.height; j++)
+					{
+						WriteNodeNeighbours(i, j);
+					}
+				}
+			}
+
+			private void WriteNodeNeighbours(int x, int y)
+			{
+				if (!IsNode(x, y))
+				{
+					return;
+				}
+
+				int rightX, rightY;
+				FindRightNeighbour(x, y, out rightX, out rightY);
+
+				int bottomX, bottomY;
+				FindBottomNeighbour(x, y, out bottomX, out bottomY);
+
+				Console.WriteLine("{0} {1} {2} {3} {4} {5}", x, y, rightX, rightY, bottomX, bottomY);
+			}
+
+			private bool IsNode(int x, int y)
+			{
+				if (x >= width)
+				{
+					return false;
+				}
+
+				if (y >= height)
+				{
+					return false;
+				}
+
+				return Equals(Node, this.nodes[y][x]);
+			}
+
+			private void FindRightNeighbour(int x, int y, out int rightX, out int rightY)
+			{
+				rightX = -1;
+				rightY = -1;
+
+				for (int i = x + 1; i < this.width; i++)
+				{
+					if (IsNode(i, y))
+					{
+						rightX = i;
+						rightY = y;
+						break;
+					}
+				}
+			}
+
+			private void FindBottomNeighbour(int x, int y, out int bottomX, out int bottomY)
+			{
+				bottomX = -1;
+				bottomY = -1;
+
+				for (int i = y + 1; i < this.height; i++)
+				{
+					if (IsNode(x, i))
+					{
+						bottomX = x;
+						bottomY = i;
+						break;
+					}
+				}
 			}
 
 			private readonly int height;
@@ -51,7 +121,6 @@ namespace CodinGamePuzzles
 			private readonly char[][] nodes;
 
 			private const char Node = '0';
-			private const char Empty = '.';
 		}
 	}
 }
